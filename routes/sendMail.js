0afc86e7271258992a4e180fe7fa00e41884ac6b
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2019-10-20 20:34:35 -0300

    send mail by get --> http://localhost:3000/sendMail?email=example@mail.com

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -4,24 +4,27 @@ let validationService = require('../services/validationService.js');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-
 router.get('/', function(req, res, next) {
 
-    if (validationService.isValidMail(req.query.email)) {
-        const msg = {
-            to: 'ncoco@fi.uba.ar',
-            from: req.query.email,
-            subject: req.query.fullName +'Consulta Patena',
-            text: req.query.message,
-            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-        };
-        sgMail.send(msg);
-        return res.status(200).send({
-            message: 'I send successful mail'
-        });
-    } else {
+    if (validationService.isValidMail(req.query.email)){
+    const email = {
+        to: 'ncoco@fi.uba.ar',
+        from: req.query.email,
+        subject: 'Sending SendGrid is Fun, enviado por ' + req.query.email,
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+
+    sgMail.send(email, function(err, json) {
+        if (err) { return console.error(err); }
+        console.log(json);
+    });
+    return res.status(200).send({
+        message:'I send successfull mail'
+    });
+    }else{
         return res.status(400).send({
-            message: 'Invalid mail'
+            message:'Invalid mail'
         });
     }
 });
