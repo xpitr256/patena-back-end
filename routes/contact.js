1dbc37eac0db4e3c30b2bfb92cc479e8f936761c
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2019-10-26 18:17:59 -0300

    add validate service test and post send mail

---
+++ routes/contact.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let router = express.Router();
+let validationService = require('../services/validationService.js');
+let mailService = require('../services/mailService.js');
+
+async function postContact(req, res) {
+    if ( validationService.isValidDataContact(req.body.email, req.body.name, req.body.message)) {
+        try {
+            await mailService.sendContactMail(req.body.email, req.body.name, req.body.message);
+            res.json({
+                message: "Contact form sent ok!"
+            });
+
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    } else {
+        res.status(400).send({
+            message:'Invalid contact information'
+        });
+    }
+}
+
+module.exports = {
+    postContact: postContact
+};
