6fab9e3e054a8be125f001db1ed9e0e072d6819b
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2019-11-10 09:32:00 -0300

    add validation file uploaded analize

--- routes/analize.js
+++ routes/analize.js
@@ -3,22 +3,27 @@ let analizeService = require ('../services/analizeService');
 
 
 function postAnalize(req, res) {
-    if ( validationService.isValidForAnalize(req.body.email, req.files)) {
-        try {
-            console.log(req.files)
-            let orderNumber = analizeService.sendOrderNumber(req.body.email, req.files.name);
-            res.json({
-                message: req.files.files.name+" --> OK:" + orderNumber.toString() + "Enviado a: " + req.body.email.toString()
-            });
+    if (req.files){
+        if ( validationService.isValidForAnalize(req.body.email, req.files)) {
+            try {
+                console.log(req.files)
+                let orderNumber = analizeService.sendOrderNumber(req.body.email, req.files.name);
+                res.json({
+                    message: req.files.files.name+" --> OK:" + orderNumber.toString() + "Enviado a: " + req.body.email.toString()
+                });
 
-        } catch (err) {
-            res.status(400).send(err);
+            } catch (err) {
+                res.status(400).send(err);
+            }
+        } else {
+            res.status(400).send({
+                message:'Invalid analize information'
+            });
         }
-    } else {
-        res.status(400).send({
-            message:'Invalid analize information'
-        });
+    }else{
+        res.status(400).send({ message: 'No files uploaded'});
     }
+
 }
 
 module.exports = {
