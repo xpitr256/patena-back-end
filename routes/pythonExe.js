dc33fd0c4a8553af1be4ec2b36496505208ddb9f
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2020-02-02 16:46:35 -0300

    changed python route test.

--- routes/pythonExe.js
+++ routes/pythonExe.js
@@ -5,11 +5,18 @@ var options = {
     args: [['AQR'], '3', '+4']
 };
 
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
 PythonShell.run('test.py', options, function (err, results) {
     if (err) throw err;
     // results is an array consisting of messages collected during execution
-    
-    console.log('results: %j', results);
+
+    let jsonString = results[0].replaceAll("'","\"")
+    let data = JSON.parse(jsonString);
+    console.log(data);
 });
 
 
--- routes/sendPythonData.js
+++ routes/sendPythonData.js
@@ -2,16 +2,40 @@ var {PythonShell} = require('python-shell');
 let express = require('express');
 let router = express.Router();
 
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
 const data = {"aminoacido":'AQR',"distancia":'3', "carga":'+4'};
 
 
 router.get('/', function(req, res, next) {
-    let pyshell = new PythonShell('/home/nico/WebstormProjects/patena-back-end/routes/sendtest.py');
+
+    var {PythonShell} = require('python-shell');
+
+    var options = {
+        mode: 'text',
+        args: [['AQR'], '3', '+4']
+    };
+
+    PythonShell.run('./routes/test.py', options, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results from python: %j', results);
+        let jsonString = results[0].replaceAll("'","\"");
+        res.json(JSON.parse(jsonString));
+    });
+
+    /*
+
+    let pyshell = new PythonShell('./sendtest.py');
 
     data["aminoacido"]=req.query.amino;
     data["distancia"]=req.query.distance;
     data["carga"]=req.query.electron;
-// sends a message to the Python script via stdin
+   // sends a message to the Python script via stdin
     pyshell.send(JSON.stringify(data));
 
     pyshell.on('message', function (message) {
@@ -27,6 +51,9 @@ router.get('/', function(req, res, next) {
         console.log('finished');
         console.log('finished');
     });
+
+
+     */
 });
 
 module.exports = router;
