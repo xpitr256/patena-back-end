a2ee4edfa2b5083e3e7b5dd3672224efcab5832f
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2020-04-01 15:22:22 -0300

    Removed testing code.

--- services/mailService.js
+++ services/mailService.js
@@ -5,16 +5,6 @@ const fs = require('fs');
 const mustache   = require('mustache');
 const translationService = require('./translationService');
 
-/***
- * TEMPORARY
- */
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
 function getCommonMailInformation(translations) {
     return {
         mr: translations.mailService.mr,
@@ -84,9 +74,6 @@ function geContactMailData(language, name, message) {
 module.exports = {
 
     sendContactMail : async function(email, name, message) {
-
-        return module.exports.sendWorkErrorMail(email,"en",'design',uuidv4());
-/*
         return new Promise((resolve, reject) => {
 
             const htmlTemplate = fs.readFileSync("./services/emailTemplates/contactMessage.html","utf-8");
@@ -108,8 +95,6 @@ module.exports = {
                 }
             });
         })
-
-        */
     },
 
     sendWorkInProgressMail : function(email, language, workType, workId) {
