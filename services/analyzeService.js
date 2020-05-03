e234cad2a96287fb7563c059b1829712564e288e
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2020-05-03 20:03:36 -0300

    Added mail notification on Design and Analysis task creation.

--- services/analyzeService.js
+++ services/analyzeService.js
@@ -1,5 +1,6 @@
 const taskService = require ('../services/taskService');
-const utilsService = require('../services/utilsService')
+const utilsService = require('../services/utilsService');
+const mailService = require('../services/mail/mailService');
 
 module.exports = {
     createAnalysis: async function(email, sequence) {
@@ -10,6 +11,10 @@ module.exports = {
             }
             const id = utilsService.uuidv4();
             await taskService.create(id, taskData, taskService.TYPE_ANALYSIS);
+            if (email) {
+                const english = 'en'; //TODO read it from client.
+                await mailService.sendWorkInProgressMail(email, english, taskService.TYPE_ANALYSIS, id);
+            }
             return id;
         } catch (e) {
             return new Error(e);
--- services/designService.js
+++ services/designService.js
@@ -1,11 +1,16 @@
 const taskService = require ('../services/taskService');
 const utilsService = require ('../services/utilsService');
+const mailService = require('../services/mail/mailService');
 
 module.exports = {
     createDesign: async function (data) {
         try {
             const id = utilsService.uuidv4();
             await taskService.create(id, data, taskService.TYPE_DESIGN);
+            if (data.email) {
+                const language = data.language ? data.language : 'en';
+                await mailService.sendWorkInProgressMail(data.email, language, taskService.TYPE_DESIGN, id);
+            }
             return id;
         } catch (e) {
             return new Error(e);
