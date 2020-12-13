58b4e26d875d07f5442d0300b36443cc81fa2a87
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2020-12-13 20:30:05 -0300

    Trying to run install.sh from the worker

--- workers/taskExecutorWorker.js
+++ workers/taskExecutorWorker.js
@@ -4,12 +4,18 @@ const throng = require("throng");
 const startWith = require("./taskExecutor");
 const logger = require("../services/log/logService");
 const Queue = require("../model/Queue");
+const shell = require('shelljs')
 
 function start() {
   startWith(Queue.getQueue());
 }
 
 logger.log("[Task Executor Worker] STARTING Task Executor worker Id=[" + process.pid + "] with WEB_CONCURRENCY: " + workers);
+logger.log("[Task Executor Worker] RUNNING PATENA install.sh");
+const { stdout, stderr, code } = shell.exec('./../patena/install.sh')
+logger.log("[Task Executor Worker] AFTER RUNNING PATENA install.sh stdout: " + stdout);
+logger.log("[Task Executor Worker] AFTER RUNNING PATENA install.sh stderr: " + stderr);
+logger.log("[Task Executor Worker] AFTER RUNNING PATENA install.sh code: " + code);
 
 // Initialize the clustered worker process
 throng({ workers, start });
