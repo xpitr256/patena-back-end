2795e42c63c25e0d170fac5c775a4d2c6cf70dee
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2020-12-13 21:27:38 -0300

    changed result.json by results.json

--- patena/patena.py
+++ patena/patena.py
@@ -1099,7 +1099,7 @@ def main():
         if testTimes:
             print_evaluation_time(total_elapsed_time,times_dict)
     if json_out:
-        with open('result.json', 'w') as fp:
+        with open('results.json', 'w') as fp:
             json.dump(json_dict, fp)
 
 if __name__ == '__main__':
