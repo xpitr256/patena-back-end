6115d94958be1e1d1e75fb0e5d7ba1e5588fdefe
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2021-01-05 21:18:47 -0300

    Fixed Patena bug related to blastWeb parameter.

--- patena/tool_functions.py
+++ patena/tool_functions.py
@@ -584,7 +584,7 @@ def chargedSearch(sequence, positionScores,config_params,inputsPath,outputsPath,
 
 
 
-def blastIt(sequence, positionScores, database, inputsPath, outputsPath, verbose, detailed_output):
+def blastIt(sequence, positionScores, database, inputsPath, outputsPath, verbose, detailed_output, blastWeb):
         global match
         ##BLAST SEARCH
         inputBlast=inputsPath+"inputBlast"
