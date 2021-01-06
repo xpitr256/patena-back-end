f054e9b01526e0b660f14dc19d2b8cd9b5c0b310
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2021-01-06 18:31:07 -0300

    trying to set blast db path

--- patena/patena.py
+++ patena/patena.py
@@ -327,7 +327,7 @@ def main():
     #  CUTOFFS, THRESHOLDS, LIMITS , DEFAULTS
     targetScore = 0.0 ### not working????
     composition = "average"
-    database = "/app/patena/uniprot_sprot"
+    database = "./patena/uniprot_sprot"
     # sequence="RANDOM"
 
 
